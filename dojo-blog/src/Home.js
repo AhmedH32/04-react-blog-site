a4b2c10e9c22208c72a3b5a04d8f145861c9e3cc
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2025-05-10 19:09:56 +0300

    feat: add BlogList component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,27 @@
+import { useState, useEffect} from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem impsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem impsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem impsum...', author: 'mario', id: 3 }
+    ])
+
+    const handleDelete = (id)=>{
+        const newBlogs = blogs.filter((blog)=>{return blog.id !== id})
+        setBlogs(newBlogs)
+    }
+
+    useEffect(()=>{
+        console.log('use effect ran')
+        console.log(blogs)
+    })
+
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            <BlogList blogs={blogs}  title={"All blogs"} handleDelete={handleDelete}></BlogList>
+
         </div>
     );
 }
